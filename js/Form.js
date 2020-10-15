df57592462afacce2f4dd1238f4a2b3a9e911b92
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-10-15 13:03:09 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -1,9 +1,6 @@
 class Form {
   constructor() {
-    this.input = createInput("Name");
-    this.button = createButton('Play');
-    this.greeting = createElement('h2');
-     }
+    
   hide(){
     this.greeting.hide();
     this.button.hide();
